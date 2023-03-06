beaf5b95ff3702119c2bb4ca914dbf73925b6566
AlinaPapsueva <98316705+AlinaPapsueva@users.noreply.github.com>
2023-03-06 22:32:03 +0300

    add configurations with Owner fo innotech_tests (lesson 17)

--- src/test/java/fastproject/innotech/lesson14/tests/setup/TestBase.java
+++ src/test/java/fastproject/innotech/lesson14/tests/setup/TestBase.java
@@ -1,36 +1,20 @@
 package fastproject.innotech.lesson14.tests.setup;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import fastproject.innotech.lesson14.tests.setup.InnotechMainPageData;
+import fastproject.innotech.lesson14.tests.config.WebDriverProvider;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import systempropertyforjenkins.lesson12.tests.Attach;
 
-import java.util.Map;
-
-// здесь лежат предустановки для Хром-драйвера
 public class TestBase {
     public InnotechMainPageData innotechMainPageData = new InnotechMainPageData();
 
     @BeforeAll
     static void setUp() {
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,  // включает режим окошко_в_окошке
-                "enableVideo", true         // включает запись видео
-        ));
-
-        Configuration.browserCapabilities = capabilities;
+        WebDriverProvider provider = new WebDriverProvider();
     }
 
     @BeforeEach
